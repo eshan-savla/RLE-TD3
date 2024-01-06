c624e61513f7529033c8752f826db97985c81e74
Eshan <eshan.savla@gmail.com>
2024-01-06 09:51:46 +0100

    fix replay buffer loading

--- src/td3_config.py
+++ src/td3_config.py
@@ -1,6 +1,6 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
-config_name = "config_6"
+config_name = "config"
 with hydra.initialize(config_path="../configs/", job_name="td3_config"):
     cfg = hydra.compose(config_name=config_name) # Option to test multiple configurations: Change the config name to your desired config file
