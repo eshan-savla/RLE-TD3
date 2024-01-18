6c8778bf6ef9c5155fe7084ddd8e26d9f70a931c
“Banane01” <raphael.aberle@web.de>
2024-01-18 17:15:38 +0100

    td3 docu update

--- src/td3.py
+++ src/td3.py
@@ -16,14 +16,16 @@ class TD3Agent:
         """
         Initializes the TD3 agent.
 
-        Args:
-            action_space (gym.Space): The action space of the environment.
-            observation_shape (tuple): The shape of the observation space.
-            gamma (float, optional): The discount factor. Defaults to 0.99.
-            tau (float, optional): The target network weight adaptation factor. Defaults to 0.001.
-            epsilon (float, optional): The exploration noise factor. Defaults to 0.05.
-            noise_clip (float, optional): The clipping range for the exploration noise. Defaults to 0.5.
-            policy_freq (int, optional): The frequency of updating the policy network. Defaults to 2.
+        Parameters:
+            - action_space (gym.Space): The action space of the environment.
+            - observation_shape (tuple): The shape of the observation space.
+            - gamma (float, optional): The discount factor. Defaults to 0.99.
+            - tau (float, optional): The target network weight adaptation factor. Defaults to 0.001.
+            - epsilon (float, optional): The exploration noise factor. Defaults to 0.05.
+            - noise_clip (float, optional): The clipping range for the exploration noise. Defaults to 0.5.
+            - policy_freq (int, optional): The frequency of updating the policy network. Defaults to 2.
+        Returns:
+            - None
         """
         self.action_space = action_space
         self.tau = tau
@@ -59,10 +61,10 @@ class TD3Agent:
         Initializes the actor and critic networks and sets their initial weights.
 
         Parameters:
-        observation_shape (int): The shape of the observation space.
+            - observation_shape (int): The shape of the observation space.
 
         Returns:
-        None
+            - None
         """
         initial_state = np.zeros([1, observation_shape])
 
@@ -81,18 +83,24 @@ class TD3Agent:
 
 
     def compute_target_q(self, rewards, next_states, dones, total_timesteps, reduce_noise: bool = False, noise_multiplicator:int = 5, min_noise_factor: float = 0.05): 
-        """_summary_:
-        Double Q-Learning
+        """
+        Computes the target Q-value for the TD3 algorithm.
+
+        _summary_:Double Q-Learning:
         Applying the 2 critic network philosophy of TD3 to the target q-value calculation
         Using the minimum of both q-values (expected cumulative reward) to reduce the overestimation bias as a parameter of the neural network
-           
-        Args:
-            rewards: reward for a given state-action pair
-            next_states: next state after taking action in a given state
-            dones: boolean value indicating if the episode is finished or not
+
+        Parameters:
+            - rewards (numpy.ndarray): Array of rewards for each transition.
+            - next_states (numpy.ndarray): Array of next states for each transition.
+            - dones (numpy.ndarray): Array of done flags for each transition.
+            - total_timesteps (int): Total number of timesteps.
+            - reduce_noise (bool, optional): Flag to reduce noise. Defaults to False.
+            - noise_multiplicator (int, optional): Multiplicator for noise reduction. Defaults to 5.
+            - min_noise_factor (float, optional): Minimum noise factor. Defaults to 0.05.
 
         Returns:
-            target_q: desired/ target estimate of q-value(cumulative reward) for a given state-action pair
+            - target_q (numpy.ndarray): Array of target Q-values for each transition.
         """
         factor = 1
         if reduce_noise:
@@ -113,40 +121,24 @@ class TD3Agent:
         target_q = rewards + (1 - dones) * next_q * self.gamma    #calulated by using the Bellman equation representing the expected cumulative reward that an agent can achieve by taking action in given state by following a policy
         
         return target_q
+    
     @staticmethod
-    def get_critic_grads(states, actions, target_qs, critic):
-
-        """_summary_:
-        Calcualation of gradients and loss for critic network by a compution q-values and estimated q-values.
+    def get_actor_grads(self, states, target_critic):    #use target critic insted of regular critic to stabilize learning process and soften issue of overestimation bias
+        """
+        Calculates the gradients of the actor network with respect to the states and target critic.
+        _summary_: Calcualation of gradients and loss for critic network by a compution q-values and estimated q-values.
         The loss is calucatted by the mean absolute difference between target Q-values and estimated q-values.
         The gradients are calculated by using the loss and the trainable variables of the critic network.
         They are clipped to prevent extreme values in rise/ falls of the gradients.
 
-        Returns:
-            gradients: gradients of the critic network
-            loss: loss of the critic network
-        """
-
-        with tf.GradientTape() as tape: 
-            critic_input = {'action': actions, 'state': states} # kommt aus Replay Buffer
-            qs = critic(critic_input) # forward pass mit den Actions und States gibt die Q-Werte aus critic nicht target_critic
-            loss = tf.reduce_mean(tf.abs(target_qs - qs)) # loss ist der Durchschnitt der absoluten Differenz zwischen den Q-Werten und den target Q-Werten. 
-        gradients = tape.gradient(loss, critic.trainable_variables) # Partielle Ableitung
-        gradients = [tf.clip_by_value(grad, -1.0, 1.0) for grad in gradients]  #gradient clipping as part of Clipped Double Q-Learning
-        return gradients, loss
-
-    def get_actor_grads(self, states, target_critic):    #use target critic insted of regular critic to stabilize learning process and soften issue of overestimation bias
-        """_summary_:
-        Calculation of the gradients and losses for the actor network. 
-        Computation of the action for given state using the actor network. 
-        Computation of Q-values using critic network. 
-        Calucation of loss as negative mean of Q-values.
-        Compuation of the gradients of the loss with respect to the trainable variables of the actor network.
+        Parameters:
+            - states: The input states to the actor network.
+            - target_critic: The target critic network used for stabilizing the learning process and reducing overestimation bias.
 
         Returns:
-            gradients: gradients of the actor network (policy)
-            loss: loss of the actor network (policy)
-        """
+            - gradients: The gradients of the actor network with respect to the trainable variables.
+            - loss: The negative mean of the Q-values calculated by the target critic network.
+            """
         with tf.GradientTape() as tape: # GradientTape speichert alle Operationen die auf Variablen ausgeführt werden
             actions = self.actor(states) # forward pass mit den States gibt die Actions gemäß der Policy aus actor nicht target_actor
             critic_input = {'action': actions, 'state': states}
@@ -159,6 +151,17 @@ class TD3Agent:
         return gradients, loss
     
     def act(self, observation, explore=True, random_action=False):
+        """
+        Selects an action based on the given observation.
+
+        Parameters:
+            - observation (numpy.ndarray): The observation from the environment.
+            - explore (bool): Whether to explore or exploit the policy. Default is True.
+            - random_action (bool): Whether to select a random action. Default is False.
+
+        Returns:
+            - a (numpy.ndarray): The selected action.
+        """
         if random_action or np.random.uniform(0, 1) < self.epsilon:
             a = self.action_space.sample()
         else:
@@ -170,6 +173,23 @@ class TD3Agent:
         return a
 
     def learn(self, states, actions, rewards, next_states, dones, step, total_timesteps):
+        """
+        Update the actor and critic networks based on the given batch of experiences.
+
+        Parameters:
+            - states (array-like): The current states.
+            - actions (array-like): The actions taken in the current states.
+            - rewards (array-like): The rewards received for the actions taken.
+            - next_states (array-like): The next states after taking the actions.
+            - dones (array-like): Whether the episode is done after taking the actions.
+            - step (int): The current step in the training process.
+            - total_timesteps (int): The total number of timesteps in the training process.
+
+        Returns:
+            - previous_actor_loss (float): The loss of the previous actor update.
+            - critics1_l (float): The loss of the first critic network.
+            - critics2_l (float): The loss of the second critic network.
+        """
         target_qs = self.compute_target_q(rewards, next_states, dones, total_timesteps)
         critic_grads1, critics1_l = self.get_critic_grads(states, actions, target_qs, self.critic_1)
         critic_grads2, critics2_l = self.get_critic_grads(states, actions, target_qs, self.critic_2)
@@ -180,12 +200,18 @@ class TD3Agent:
             actor_grads, self.previous_actor_loss = self.get_actor_grads(states, self.target_critic_1)
             self.actor_optimizer.apply_gradients(zip(actor_grads, self.actor.trainable_variables))
             self.target_update()
-          
+            
         return self.previous_actor_loss, critics1_l, critics2_l
-    
+
     def target_update(self):
         """
-        Update the target networks by copying the weights from the main networks with a soft update.
+        Update the target networks by copying the weights from the output networks.
+
+        Parameters:
+            - None
+
+        Returns:
+            - None
         """
         TD3Agent.update_target(self.target_actor, self.actor, self.tau)
         TD3Agent.update_target(self.target_critic_1, self.critic_1, self.tau)
@@ -193,18 +219,21 @@ class TD3Agent:
 
 
     @staticmethod     
-        # Decorator is used to define a method that belongs to the class itself rather than an instance of the class. 
-        # It allows for the creation of utility functions or operations that are related to the class but do not require access to instance-specific data. 
-        # Their methods can be called directly on the class without the need for an instance
+    # Decorator is used to define a method that belongs to the class itself rather than an instance of the class. 
+    # It allows for the creation of utility functions or operations that are related to the class but do not require access to instance-specific data. 
+    # Their methods can be called directly on the class without the need for an instance
     def update_target(model_target, model_ref, tau=0.005):
-        """_summary_:
+        """
         Update of the target networks can be done periodically by slowly blending their weights with the weights of the regular networks.
         Slow updates smoothen the learning process and provides more consistent and accurate q-value estimates.
-        
-        Args:
-            model_target: target network to be updated
-            model_ref: regular network to be used as reference
-            tau: blending factor, default value is 0.005      --TODO: check if default value is correct with regard to the paper
+
+        Parameters:
+            - model_target (tf.keras.Model): The target model to be updated.
+            - model_ref (tf.keras.Model): The reference model whose weights will be used for the update.
+            - tau (float): The interpolation parameter for the update. Default is 0.0, which means a hard update.
+
+        Returns:
+            - None
         """
         new_weights = [tau * ref_weight + (1 - tau) * target_weight for (target_weight, ref_weight) in
                        list(zip(model_target.get_weights(), model_ref.get_weights()))]
@@ -212,9 +241,14 @@ class TD3Agent:
 
     def save_weights(self):
         """
-        Save the weights of the TD3 agent's actor and critic networks, as well as the target networks.
-        The weights are saved in separate .npz files in the specified save directory.
-        If no save directory is specified, a new directory is created with the current date and time.
+        Save the weights of the TD3 agent's networks.
+        The weights are saved in separate files using the current timestamp as the directory name.
+
+        Parameters:
+            - None
+
+        Returns:
+            - None
         """
         if self.save_dir is None:
             now = datetime.now()
@@ -232,18 +266,15 @@ class TD3Agent:
 
     def load_weights(self, use_latest:bool=True, load_dir:str=None, lock_weights:bool=False):
         """
-        Loads the weights of the TD3 agent from the specified directory.
+        Loads the weights of the TD3 agent from a specified directory.
 
         Parameters:
-        - use_latest (bool): If True, loads the weights from the latest directory in the weights_path.
-                                If False, loads the weights from the specified load_dir.
-                                Default is True.
-        - load_dir (str): The directory path from which to load the weights.
-                            If None, the weights_path will be used.
-                            Default is None.
-        - lock_weights (bool): If True, locks the weights of the actor, critic, and target networks.
-                                This is irreversible.
-                                Default is False.
+            - use_latest (bool): If True, loads the weights from the latest directory in the weights_path.
+            - load_dir (str): The directory path from which to load the weights.
+            - lock_weights (bool): If True, locks the trainable status of the actor and critic models.
+
+        Returns:
+            - None
         """
         if use_latest:
             load_dir = os.path.join(cfg.TD3Agent.weights_path,max(os.listdir(cfg.TD3Agent.weights_path))) + "/"
