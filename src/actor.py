e93c0a3bbb577b50106dfcfa02d3d8301b66e0fd
“Banane01” <raphael.aberle@web.de>
2024-01-18 16:39:26 +0100

    updated critic doku

--- src/actor.py
+++ src/actor.py
@@ -2,25 +2,25 @@ import tensorflow as tf
 
 class Actor(tf.keras.layers.Layer):
     def __init__(self, units=(400, 300), n_actions=2, stddev=0.00005, **kwargs):
-            """
-            Initialize the Actor network.
+        """
+        Initialize the Actor network.
 
-            Parameters:
-                units (tuple): A tuple specifying the number of units/neurons in each hidden layer.
-                n_actions (int): The dimension of the action space.
-                stddev (float): The standard deviation for the kernel initializer.
-                **kwargs: Additional keyword arguments to be passed to the base class.
+        Parameters:
+            units (tuple): A tuple specifying the number of units/neurons in each hidden layer.
+            n_actions (int): The dimension of the action space.
+            stddev (float): The standard deviation for the kernel initializer.
+            **kwargs: Additional keyword arguments to be passed to the base class.
 
-            Returns:
-                None
-            """
-            super(Actor, self).__init__(**kwargs)
-            self.layers = []
-            for i, u in enumerate(units):
-                self.layers.append(tf.keras.layers.Dense(u, activation=tf.nn.leaky_relu,
-                                                         kernel_initializer=tf.keras.initializers.glorot_normal())) # two layers with neurons
-            last_init = tf.random_normal_initializer(stddev=stddev)
-            self.layers.append(tf.keras.layers.Dense(n_actions, activation='tanh', kernel_initializer=last_init)) # output layer with the dimension of the action space
+        Returns:
+            None
+        """
+        super(Actor, self).__init__(**kwargs)
+        self.layers = []
+        for i, u in enumerate(units):
+            self.layers.append(tf.keras.layers.Dense(u, activation=tf.nn.leaky_relu,
+                                                        kernel_initializer=tf.keras.initializers.glorot_normal())) # two layers with neurons
+        last_init = tf.random_normal_initializer(stddev=stddev)
+        self.layers.append(tf.keras.layers.Dense(n_actions, activation='tanh', kernel_initializer=last_init)) # output layer with the dimension of the action space
 
     def call(self, inputs, **kwargs):
         """
--- src/critic.py
+++ src/critic.py
@@ -2,38 +2,62 @@ import tensorflow as tf
 
 class Critic(tf.keras.layers.Layer):
     def __init__(self, state_units=(400, 300), action_units=(300,), units=(150,), stddev=0.00005, **kwargs):
+        """
+        Initializes the Critic class.
+
+        Parameters:
+            state_units (tuple): A tuple specifying the number of units in each state layer.
+            action_units (tuple): A tuple specifying the number of units in each action layer.
+            units (tuple): A tuple specifying the number of units in each Q-value layer.
+            stddev (float): The standard deviation for the random normal initializer.
+            **kwargs: Additional keyword arguments to be passed to the base class constructor.
+
+        Returns:
+            None
+        """
         super(Critic, self).__init__(**kwargs)
         self.layers_state = []
         for u in state_units:
             self.layers_state.append(tf.keras.layers.Dense(u, activation=tf.nn.leaky_relu,
-                                                           kernel_initializer=tf.keras.initializers.glorot_normal())) # Layers für die States
+                                                            kernel_initializer=tf.keras.initializers.glorot_normal())) # Layers for states
 
         self.layers_action = []
         for u in action_units:
             self.layers_action.append(tf.keras.layers.Dense(u, activation=tf.nn.leaky_relu,
-                                                            kernel_initializer=tf.keras.initializers.glorot_normal()))  # Layers für die Actions
+                                                            kernel_initializer=tf.keras.initializers.glorot_normal()))  # Layers for actions
 
         self.layers = []
         for u in units:
             self.layers.append(tf.keras.layers.Dense(u, activation=tf.nn.leaky_relu,
-                                                     kernel_initializer=tf.keras.initializers.glorot_normal())) # Layers für die Q-Werte
+                                                        kernel_initializer=tf.keras.initializers.glorot_normal())) # Layers for q-values
         last_init = tf.random_normal_initializer(stddev=stddev) 
-        self.layers.append(tf.keras.layers.Dense(1, kernel_initializer=last_init)) # letzte Layer welche den Q-Wert ausgibt
+        self.layers.append(tf.keras.layers.Dense(1, kernel_initializer=last_init)) # last layer which outputs the Q-value
 
         self.add = tf.keras.layers.Add()
          
     def call(self, inputs, **kwargs):
+        """
+        Forward pass of the critic network.
+
+        Args:
+            inputs (dict): Dictionary containing the input tensors.
+                - 'action': Tensor representing the action input.
+                - 'state': Tensor representing the state input.
+
+        Returns:
+            outputs (Tensor): Tensor representing the q-values output.
+        """
         p_action = inputs['action']
         p_state = inputs['state']
 
         for l in self.layers_action:
-            p_action = l(p_action) # forward pass für die Actions
+            p_action = l(p_action) # forward pass for actions
 
         for l in self.layers_state:
-            p_state = l(p_state) # forward pass für die States
+            p_state = l(p_state) # forward pass for states
 
         outputs = self.add([p_state, p_action])
         for l in self.layers:
-            outputs = l(outputs) # forward pass für die Q-Werte
+            outputs = l(outputs) # forward pass for q-values
 
-        return outputs # Q-Wert
+        return outputs # q-value
